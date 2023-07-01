d20f2c34045fb31f1ffdac1ddcd0dff9657a2be2
baananou <bananoumohamed@live.fr>
2023-07-01 10:51:31 +0100

    first commit

---
+++ Backend/app.py
@@ -0,0 +1,35 @@
+# Required imports
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, initialize_app, db
+from flask_cors import CORS, cross_origin
+
+# Initialize Flask app
+app = Flask(__name__)
+CORS(app)
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred, {
+    'databaseURL': 'https://c2ipfe-default-rtdb.europe-west1.firebasedatabase.app/'  # Replace with your Firebase Realtime Database URL
+})
+
+# Get a reference to the Firebase Realtime Database
+ref = db.reference()
+
+@app.route('/data', methods=['GET'])
+@cross_origin()
+def get_data():
+    key = request.args.get('key')  # Get the value of the 'key' query parameter
+
+    if key is None:
+        return jsonify({'error': 'Key parameter is required.'}), 400
+
+    data = ref.get()
+    if key in data:
+        value = data[key]
+        return jsonify({key: value}), 200
+    else:
+        return jsonify({'error': 'Key not found.'}), 404
+
+if __name__ == '__main__':
+    app.run()
